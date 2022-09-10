2593e6ad0a718fe94b6ddb07bea576a1ba8724ec
Jan_Balangue <jan.a.balangue@gmail.com>
2022-09-10 07:38:11 -0500

    add service impl

--- src/main/java/io/github/janbalangue/restapi/impl/StudentServiceImpl.java
+++ src/main/java/io/github/janbalangue/restapi/impl/StudentServiceImpl.java
@@ -1,41 +1,46 @@
 package io.github.janbalangue.restapi.impl;
 
 import io.github.janbalangue.restapi.model.Student;
+import io.github.janbalangue.restapi.repository.StudentRepository;
 import io.github.janbalangue.restapi.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
 
 public class StudentServiceImpl implements StudentService {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     @Override
     public List<Student> findAll() {
-        return null;
+        return studentRepository.findAll();
     }
 
 
     @Override
     public Optional<Student> findById(String id) {
-        return Optional.empty();
+        return studentRepository.findById(id);
     }
 
     @Override
     public Student findByFirstNameAndLastName(String firstName, String lastName) {
-        return null;
+        return studentRepository.findByFirstNameAndLastName(firstName, lastName);
     }
 
     @Override
     public List<Student> findByGrade(String grade) {
-        return null;
+        return studentRepository.findByGrade(grade);
     }
 
     @Override
     public Student saveOrUpdateStudent(Student student) {
-        return null;
+        return studentRepository.save(student);
     }
 
     @Override
     public void deleteStudentById(String id) {
-
+        studentRepository.deleteById(id);
     }
 }
